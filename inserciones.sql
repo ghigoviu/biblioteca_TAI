4a54a66b766c4d1e5b7e9c021d6f6669788de25f
ghigoviu <ghigoviu@gmail.com>
2022-03-28 15:56:00 -0600

    Modificación de vistas, agregar login y formatear tabla

---
+++ inserciones.sql
@@ -0,0 +1,20 @@
+//Inserciones de Biblioteca Borges 
+INSERT INTO `libros` (`id`, `titulo`, `autor`, `categoría`, `descripcion`, `created_at`, `updated_at`) 
+VALUES ('1', 'El jardín de los senderos que se bifurcan', 'Jorge Luis Borges', 'Fantasía', 
+'Cuento escrito en 1941. Representa así la imagen del universo como la concebía Tsu\'ui Pên y, si aceptamos la hipótesis de Everett, el mundo subatómico también participa en esta concepción.', 
+CURRENT_DATE(), CURRENT_DATE());
+
+INSERT INTO `libros` (`id`, `titulo`, `autor`, `categoría`, `descripcion`, `created_at`, `updated_at`) 
+VALUES ('2', 'Las indias negras', 'Julio Verne', 'Misterio', 
+'Novela clásica de Verne, el ingeniero Santiago Starr visita luego de muchos años la mina abandonada de Aberfoyle donde parece haber una oportunidad para revivir sus días de gloria así como una misteriosa amenaza.', 
+CURRENT_DATE(), CURRENT_DATE());
+
+INSERT INTO `libros` (`id`, `titulo`, `autor`, `categoría`, `descripcion`, `created_at`, `updated_at`) 
+VALUES ('3', 'La llamada de Cthulu', 'H. P. Lovecraft', 'Horror', 
+'Historia donde Randolph Carter se encuentra con los escritos de su abuelo, que relatan una inquietante amenaza mas allá de la imaginación.', 
+CURRENT_DATE(), CURRENT_DATE());
+
+INSERT INTO `libros` (`id`, `titulo`, `autor`, `categoría`, `descripcion`, `created_at`, `updated_at`) 
+VALUES ('4', 'Cumbres Borrascosas', 'Emily Brontë', 'Drama', 
+'La intrigante historia de Catherine y Heathcliff .Situada en los sombríos páramos de Yorkshire.', 
+CURRENT_DATE(), CURRENT_DATE());
--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -4,18 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Catalogo</title>
 </head>
 <body>
+    @if (Route::has('login'))
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        @auth
+            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+        @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            @endif
+        @endauth
+     <!--   Cabecera--> 
+    </div>
+    @endif
     <table>
         @foreach ($productos as $pro)
         <tr>
             <td>{{$pro->titulo}} </td>
-        </tr>
-        <tr>
             <td>{{$pro->autor}} </td>
+            <td>{{$pro->descripcion}} </td>
         </tr>
         @endforeach
     </table> 
+    <!--   Footer>
 </body>
 </html>
